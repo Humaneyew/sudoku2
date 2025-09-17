f865b144773cbc5de4ed8851724f30621f51e944
UZOR <36662799+Humaneyew@users.noreply.github.com>
2025-09-17 15:58:18 +0400

    Add generated localizations support

--- lib/models.dart
+++ lib/models.dart
@@ -3,7 +3,7 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sudoku2/flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'puzzles.dart';
